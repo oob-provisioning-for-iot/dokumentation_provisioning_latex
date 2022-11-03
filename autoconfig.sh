ac8c49ad0240fb52609c8d4c0f2ff0df2a6a2f23
MaxProImCt <96023925+MaxProImCt@users.noreply.github.com>
2022-11-03 23:03:55 +0100

    Add files via upload
    autoconfig skript 1.0 für die Gerätekonfigurationen bezüglich der Cloud, die ersten 2 Variablen funktionieren und die zugehörigen if klauseln ebenfalls

---
+++ autoconfig.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ -n "$1" ]
+then
+	sed -i "3 s/"false"/"$1"/" copytestsed
+fi
+
+if [ -n "$2" ]
+then
+	sed -i "4 s/"Azure"/"$2"/" copytestsed
+fi
+
+sed -i "5 s/false/$1/" copytestsed
+sed -i "6 s/false/$1/" copytestsed
+sed -i "7 s/false/$1/" copytestsed
+sed -i "8 s/false/$1/" copytestsed
+sed -i "9 s/false/$1/" copytestsed
+sed -i "10 s/false/$1/" copytestsed
+sed -i "11 s/false/$1/" copytestsed
+sed -i "12 s/false/$1/" copytestsed
+sed -i "13 s/false/$1/" copytestsed
+sed -i "14 s/false/$1/" copytestsed
+sed -i "15 s/false/$1/" copytestsed
+sed -i "16 s/false/$1/" copytestsed
+sed -i "17 s/false/$1/" copytestsed
+sed -i "18 s/false/$1/" copytestsed
+sed -i "19 s/false/$1/" copytestsed
+sed -i "20 s/false/$1/" copytestsed
+sed -i "21 s/false/$1/" copytestsed
+sed -i "22 s/false/$1/" copytestsed
+sed -i "23 s/false/$1/" copytestsed
+sed -i "24 s/false/$1/" copytestsed
+sed -i "25 s/false/$1/" copytestsed
+sed -i "26 s/false/$1/" copytestsed
+sed -i "27 s/false/$1/" copytestsed
+sed -i "28 s/false/$1/" copytestsed
+sed -i "29 s/false/$1/" copytestsed
+sed -i "30 s/false/$1/" copytestsed
+sed -i "31 s/false/$1/" copytestsed
+sed -i "32 s/false/$1/" copytestsed
+sed -i "33 s/false/$1/" copytestsed
+sed -i "34 s/false/$1/" copytestsed
+sed -i "35 s/false/$1/" copytestsed
